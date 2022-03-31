6c7db4a0983c6547ddfbe2cfc9cc53a0d3f27178
Pawel Zmarlak <zmarlakpawel@gmail.com>
2022-03-31 23:00:19 +0100

    'quickfix'

--- scripts/converter.js
+++
@@ -1,9 +0,0 @@
-//access the csv file and convert it to JSON
-const csvFilePath='./../resources/interactions.csv'
-const csv=require('csvtojson')
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    console.log(jsonObj);
-    let foo = jsonObj;
-})
--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
